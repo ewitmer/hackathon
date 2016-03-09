bb04d7102e8443be5149e5570330e8c0e6815362
Erin Witmer <ecwitmer@gmail.com>
2016-03-08 20:58:37 -0500

    File structure with assets

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-var recipes = []
+
+var spaghettiMeatballs = {
+  picture: 
+  pasta: 1
+  sauce: 1
+  groundBeef: 1
+}
+
+var cheeseBurgers = {
+  picture :
+  groundBeef: 1
+  hamburgerBuns: 4
+  cheese: 1
+}
+
+var spinachSalad = {
+  picture: 
+  spinach: 1
+  chicken: 1
+  tomato:  1
+}
+
+var allRecipes = [spaghettiMeatballs, cheeseBurgers, spinachSalad]
+var selectedRecipes = [spaghettiMeatballs, cheeseBurgers]
